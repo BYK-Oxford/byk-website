69c2dab02e8fb55b8b8d58c12aed5effd08014eb
Gurung-Manish <manishgrg496@gmail.com>
2025-05-16 11:22:43 +0100

    added hero section

---
+++ src/app/home/page.tsx
@@ -0,0 +1,11 @@
+// Home.jsx
+import React from "react";
+import Hero from "@/app/home/components/hero";
+
+export default function Home() {
+  return (
+    <div className="bg-white">
+      <Hero />
+    </div>
+  );
+}
