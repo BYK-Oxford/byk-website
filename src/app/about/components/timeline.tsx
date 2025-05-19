df5f220165c02e75df800b21012f6737972de802
Gurung-Manish <manishgrg496@gmail.com>
2025-05-19 14:34:58 +0100

    more sections for about

---
+++ src/app/about/components/timeline.tsx
@@ -0,0 +1,69 @@
+const timeline = [
+  {
+    name: "Initial concept and planning",
+    description:
+      "Brainstormed ideas, conducted market research, and laid the groundwork for the company's vision and strategy.",
+    date: "Jan 2024",
+    dateTime: "2024-01",
+  },
+  {
+    name: "First client acquired",
+    description:
+      "Secured the company’s first client, marking the beginning of real-world application and feedback.",
+    date: "Oct 2024",
+    dateTime: "2024-10",
+  },
+  {
+    name: "Product development underway",
+    description:
+      "Started building the initial version of the product with a focus on customer needs and usability.",
+    date: "Mar 2025",
+    dateTime: "2025-03",
+  },
+
+  {
+    name: "Product development",
+    description:
+      "Started building the initial version of the product with a focus on customer needs and usability.",
+    date: "Mar 2025",
+    dateTime: "2025-03",
+  },
+];
+
+export default function Timeline() {
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-8 overflow-hidden lg:mx-0 lg:max-w-none lg:grid-cols-4">
+          {timeline.map((item) => (
+            <div key={item.name}>
+              <time
+                dateTime={item.dateTime}
+                className="flex items-center text-sm/6 font-semibold text-indigo-600"
+              >
+                <svg
+                  viewBox="0 0 4 4"
+                  aria-hidden="true"
+                  className="mr-4 size-1 flex-none"
+                >
+                  <circle r={2} cx={2} cy={2} fill="currentColor" />
+                </svg>
+                {item.date}
+                <div
+                  aria-hidden="true"
+                  className="absolute -ml-2 h-px w-screen -translate-x-full bg-gray-900/10 sm:-ml-4 lg:static lg:-mr-6 lg:ml-8 lg:w-auto lg:flex-auto lg:translate-x-0"
+                />
+              </time>
+              <p className="mt-6 text-lg/8 font-semibold tracking-tight text-gray-900">
+                {item.name}
+              </p>
+              <p className="mt-1 text-base/7 text-gray-600">
+                {item.description}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,15 @@
 import Hero from "@/app/about/components/hero";
+import CEO from "@/app/about/components/ceo";
+import Timeline from "@/app/about/components/timeline";
+import Abouts from "@/app/about/components/about";
 
 export default function About() {
   return (
     <div className="bg-white">
       <Hero />
+      <Abouts />
+      <Timeline />
+      <CEO />
     </div>
   );
 }
