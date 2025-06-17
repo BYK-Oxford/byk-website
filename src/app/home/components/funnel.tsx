d1ffceda9261577d0ca70115c5dc508afc7cb7c7
Gurung-Manish <manishgrg496@gmail.com>
2025-06-17 18:06:01 +0100

    updating funnel and map together

--- src/app/home/components/funnel.tsx
+++ src/app/home/components/funnel.tsx
@@ -68,28 +68,24 @@ export default function Funnel() {
   const sizes = [50, 80, 100];
   return (
     <div className="bg-white py-12 sm:py-16">
-      <div className="mx-auto max-w-5xl px-4 lg:px-6">
-        <div className="mx-auto max-w-xl text-center mb-10">
-          <h2
-            ref={headingRef}
-            className="text-sm font-semibold text-indigo-600"
-          >
-            Heading
-          </h2>
-          <p
-            ref={subHeadingRef}
-            className="mt-2 text-2xl font-semibold tracking-tight text-gray-900 sm:text-3xl"
-          >
-            How your Business looks
-          </p>
-          <p ref={paraRef} className="mt-4 text-base text-gray-600">
-            We plan to maximise your work and money by efficient funneling.
-          </p>
-        </div>
+      <div className="mx-auto max-w-xl text-center mb-10">
+        <h2 ref={headingRef} className="text-sm font-semibold text-indigo-600">
+          Heading
+        </h2>
+        <p
+          ref={subHeadingRef}
+          className="mt-2 text-2xl font-semibold tracking-tight text-gray-900 sm:text-3xl"
+        >
+          How your Business looks
+        </p>
+        <p ref={paraRef} className="mt-4 text-base text-gray-600">
+          We plan to maximise your work and money by efficient funneling.
+        </p>
+      </div>
 
-        {/* Funnel Layers */}
-        <div className="flex flex-col items-center gap-[10px] transform scale-70 md:scale-100">
-          {/* <div className="flex items-center gap-4">
+      {/* Funnel Layers */}
+      <div className="flex flex-col items-center gap-[10px] transform scale-70 md:scale-100">
+        {/* <div className="flex items-center gap-4">
             <div className="w-40 text-right pr-4">
               <p className="text-gray-700 font-semibold">Sales Pipeline</p>
             </div>
@@ -141,198 +137,197 @@ export default function Funnel() {
             </div>
           </div> */}
 
+        <div
+          ref={(el) => {
+            funnelLayers.current[0] = el;
+          }}
+          className="full-funnel"
+          style={{
+            width: "500px",
+            position: "relative",
+            transition: "transform 0.3s ease",
+          }}
+          id="funnel-layer-0"
+        >
+          {/* Top Cap - simple ellipse */}
           <div
-            ref={(el) => {
-              funnelLayers.current[0] = el;
-            }}
-            className="full-funnel"
+            className="funnel-top"
             style={{
-              width: "500px",
-              position: "relative",
-              transition: "transform 0.3s ease",
+              height: "40px",
+              width: "500px", // slightly narrower or equal to funnel top width
+              background: "#818cf8",
+              borderRadius: "50% / 100%",
+              margin: "0 auto",
+              marginBottom: "-20px", // overlap with funnel body
+              boxShadow: "0 8px 20px rgb(99 102 241 / 0.3)",
+              position: "relative", // Make zIndex effective
+              zIndex: 10,
             }}
-            id="funnel-layer-0"
+          />
+          {/* Funnel Body */}
+          <div
+            className="funnel-layer bg-indigo-500"
+            style={{ height: "120px", width: "500px" }}
           >
-            {/* Top Cap - simple ellipse */}
-            <div
-              className="funnel-top"
-              style={{
-                height: "40px",
-                width: "500px", // slightly narrower or equal to funnel top width
-                background: "#818cf8",
-                borderRadius: "50% / 100%",
-                margin: "0 auto",
-                marginBottom: "-20px", // overlap with funnel body
-                boxShadow: "0 8px 20px rgb(99 102 241 / 0.3)",
-                position: "relative", // Make zIndex effective
-                zIndex: 10,
-              }}
-            />
-            {/* Funnel Body */}
-            <div
-              className="funnel-layer bg-indigo-500"
-              style={{ height: "120px", width: "500px" }}
-            >
-              <div className="text-xl font-bold">Sales Pipeline</div>
-            </div>
-            {/* Bottom Cap - simple ellipse */}
-            <div
-              className="bg-indigo-500"
-              style={{
-                height: "40px",
-                width: "400px",
-                borderRadius: "50% / 100%",
-                margin: "0 auto",
-                marginTop: "-36px", // overlap with funnel body
-                boxShadow: "0 8px 20px rgb(99 102 241 / 0.3)",
-                zIndex: "20",
-              }}
-            />
+            <div className="text-xl font-bold">Sales Pipeline</div>
           </div>
+          {/* Bottom Cap - simple ellipse */}
           <div
-            ref={(el) => {
-              funnelLayers.current[1] = el;
-            }}
-            className="full-funnel"
+            className="bg-indigo-500"
             style={{
+              height: "40px",
               width: "400px",
-              position: "relative",
-              transition: "transform 0.3s ease",
+              borderRadius: "50% / 100%",
+              margin: "0 auto",
+              marginTop: "-36px", // overlap with funnel body
+              boxShadow: "0 8px 20px rgb(99 102 241 / 0.3)",
+              zIndex: "20",
             }}
-            id="funnel-layer-0"
-          >
-            {/* Top Cap - simple ellipse */}
-            <div
-              className="funnel-top"
-              style={{
-                height: "40px",
-                width: "400px", // slightly narrower or equal to funnel top width
-                background: "#818cf8",
-                borderRadius: "50% / 100%",
-                margin: "0 auto",
-                marginBottom: "-20px", // overlap with funnel body
-                boxShadow: "0 8px 20px rgb(99 102 241 / 0.3)",
-                position: "relative", // Make zIndex effective
-                zIndex: 10,
-              }}
-            />
-            {/* Funnel Body */}
-            <div
-              className="funnel-layer bg-indigo-500"
-              style={{ height: "120px", width: "400px" }}
-            >
-              <div className="text-xl font-bold">Revenue Conversion</div>
-            </div>
-            {/* Bottom Cap - simple ellipse */}
-            <div
-              className="bg-indigo-500"
-              style={{
-                height: "40px",
-                width: "320px",
-                borderRadius: "50% / 100%",
-                margin: "0 auto",
-                marginTop: "-36px", // overlap with funnel body
-                boxShadow: "0 8px 20px rgb(99 102 241 / 0.3)",
-                zIndex: "20",
-              }}
-            />
-          </div>
+          />
+        </div>
+        <div
+          ref={(el) => {
+            funnelLayers.current[1] = el;
+          }}
+          className="full-funnel"
+          style={{
+            width: "400px",
+            position: "relative",
+            transition: "transform 0.3s ease",
+          }}
+          id="funnel-layer-0"
+        >
+          {/* Top Cap - simple ellipse */}
           <div
-            ref={(el) => {
-              funnelLayers.current[2] = el;
-            }}
-            className="full-funnel"
+            className="funnel-top"
             style={{
-              width: "300px",
-              position: "relative",
-              transition: "transform 0.3s ease",
+              height: "40px",
+              width: "400px", // slightly narrower or equal to funnel top width
+              background: "#818cf8",
+              borderRadius: "50% / 100%",
+              margin: "0 auto",
+              marginBottom: "-20px", // overlap with funnel body
+              boxShadow: "0 8px 20px rgb(99 102 241 / 0.3)",
+              position: "relative", // Make zIndex effective
+              zIndex: 10,
             }}
-            id="funnel-layer-0"
-          >
-            {/* Top Cap - simple ellipse */}
-            <div
-              className="funnel-top"
-              style={{
-                height: "40px",
-                width: "300px", // slightly narrower or equal to funnel top width
-                background: "#818cf8",
-                borderRadius: "50% / 100%",
-                margin: "0 auto",
-                marginBottom: "-20px", // overlap with funnel body
-                boxShadow: "0 8px 20px rgb(99 102 241 / 0.3)",
-                position: "relative", // Make zIndex effective
-                zIndex: 10,
-              }}
-            />
-            {/* Funnel Body */}
-            <div
-              className="funnel-layer bg-indigo-500"
-              style={{ height: "120px", width: "300px" }}
-            >
-              <div className="text-xl font-bold">EBITDA</div>
-            </div>
-            {/* Bottom Cap - simple ellipse */}
-            <div
-              className="bg-indigo-500"
-              style={{
-                height: "40px",
-                width: "240px",
-                borderRadius: "50% / 100%",
-                margin: "0 auto",
-                marginTop: "-36px", // overlap with funnel body
-                boxShadow: "0 8px 20px rgb(99 102 241 / 0.3)",
-                zIndex: "20",
-              }}
-            />
-          </div>
+          />
+          {/* Funnel Body */}
           <div
-            ref={(el) => {
-              funnelLayers.current[2] = el;
-            }}
-            className="full-funnel"
+            className="funnel-layer bg-indigo-500"
+            style={{ height: "120px", width: "400px" }}
+          >
+            <div className="text-xl font-bold">Revenue Conversion</div>
+          </div>
+          {/* Bottom Cap - simple ellipse */}
+          <div
+            className="bg-indigo-500"
             style={{
+              height: "40px",
+              width: "320px",
+              borderRadius: "50% / 100%",
+              margin: "0 auto",
+              marginTop: "-36px", // overlap with funnel body
+              boxShadow: "0 8px 20px rgb(99 102 241 / 0.3)",
+              zIndex: "20",
+            }}
+          />
+        </div>
+        <div
+          ref={(el) => {
+            funnelLayers.current[2] = el;
+          }}
+          className="full-funnel"
+          style={{
+            width: "300px",
+            position: "relative",
+            transition: "transform 0.3s ease",
+          }}
+          id="funnel-layer-0"
+        >
+          {/* Top Cap - simple ellipse */}
+          <div
+            className="funnel-top"
+            style={{
+              height: "40px",
+              width: "300px", // slightly narrower or equal to funnel top width
+              background: "#818cf8",
+              borderRadius: "50% / 100%",
+              margin: "0 auto",
+              marginBottom: "-20px", // overlap with funnel body
+              boxShadow: "0 8px 20px rgb(99 102 241 / 0.3)",
+              position: "relative", // Make zIndex effective
+              zIndex: 10,
+            }}
+          />
+          {/* Funnel Body */}
+          <div
+            className="funnel-layer bg-indigo-500"
+            style={{ height: "120px", width: "300px" }}
+          >
+            <div className="text-xl font-bold">EBITDA</div>
+          </div>
+          {/* Bottom Cap - simple ellipse */}
+          <div
+            className="bg-indigo-500"
+            style={{
+              height: "40px",
               width: "240px",
-              position: "relative",
-              transition: "transform 0.3s ease",
+              borderRadius: "50% / 100%",
+              margin: "0 auto",
+              marginTop: "-36px", // overlap with funnel body
+              boxShadow: "0 8px 20px rgb(99 102 241 / 0.3)",
+              zIndex: "20",
             }}
-            id="funnel-layer-0"
+          />
+        </div>
+        <div
+          ref={(el) => {
+            funnelLayers.current[2] = el;
+          }}
+          className="full-funnel"
+          style={{
+            width: "240px",
+            position: "relative",
+            transition: "transform 0.3s ease",
+          }}
+          id="funnel-layer-0"
+        >
+          {/* Top Cap - simple ellipse */}
+          <div
+            className="funnel-top"
+            style={{
+              height: "40px",
+              width: "240px", // slightly narrower or equal to funnel top width
+              background: "#818cf8",
+              borderRadius: "50% / 100%",
+              margin: "0 auto",
+              marginBottom: "-20px", // overlap with funnel body
+              boxShadow: "0 8px 20px rgb(99 102 241 / 0.3)",
+              position: "relative", // Make zIndex effective
+              zIndex: 10,
+            }}
+          />
+          {/* Funnel Body */}
+          <div
+            className="funnel-layer bg-indigo-500"
+            style={{ height: "120px", width: "240px" }}
           >
-            {/* Top Cap - simple ellipse */}
-            <div
-              className="funnel-top"
-              style={{
-                height: "40px",
-                width: "240px", // slightly narrower or equal to funnel top width
-                background: "#818cf8",
-                borderRadius: "50% / 100%",
-                margin: "0 auto",
-                marginBottom: "-20px", // overlap with funnel body
-                boxShadow: "0 8px 20px rgb(99 102 241 / 0.3)",
-                position: "relative", // Make zIndex effective
-                zIndex: 10,
-              }}
-            />
-            {/* Funnel Body */}
-            <div
-              className="funnel-layer bg-indigo-500"
-              style={{ height: "120px", width: "240px" }}
-            >
-              <div className="text-xl font-bold">Net Profit</div>
-            </div>
-            {/* Bottom Cap - simple ellipse */}
-            <div
-              className="bg-indigo-500"
-              style={{
-                height: "40px",
-                width: "190px",
-                borderRadius: "50% / 100%",
-                margin: "0 auto",
-                marginTop: "-36px", // overlap with funnel body
-                boxShadow: "0 8px 20px rgb(99 102 241 / 0.3)",
-                zIndex: "20",
-              }}
-            />
+            <div className="text-xl font-bold">Net Profit</div>
           </div>
+          {/* Bottom Cap - simple ellipse */}
+          <div
+            className="bg-indigo-500"
+            style={{
+              height: "40px",
+              width: "190px",
+              borderRadius: "50% / 100%",
+              margin: "0 auto",
+              marginTop: "-36px", // overlap with funnel body
+              boxShadow: "0 8px 20px rgb(99 102 241 / 0.3)",
+              zIndex: "20",
+            }}
+          />
         </div>
       </div>
     </div>
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,18 +4,16 @@ import React from "react";
 import Hero from "@/app/home/components/hero";
 import Stats from "@/app/home/components/stats";
 import Screen from "@/app/home/components/screen";
-import WorldMap from "@/app/home/components/map";
 import Contact from "@/app/home/components/contact";
-import Funnel from "@/app/home/components/funnel";
+import FunnelBody from "@/app/home/components/funnelBody";
 
 export default function Home() {
   return (
     <div>
-      <WorldMap />
       <Hero />
       <Stats />
       <Screen />
-      <Funnel />
+      <FunnelBody />
       <Contact />
     </div>
   );
