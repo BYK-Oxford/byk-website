4c8d823903e07401a710db43f6f2c26baaa24689
Gurung-Manish <manishgrg496@gmail.com>
2025-06-20 22:41:25 +0100

    updated pictures

--- src/app/about/components/mission.tsx
+++ src/app/about/components/mission.tsx
@@ -279,7 +279,7 @@ export default function Mission() {
               >
                 <img
                   alt=""
-                  src="https://images.unsplash.com/photo-1590650516494-0c8e4a4dd67e?&auto=format&fit=crop&crop=center&w=560&h=560&q=90"
+                  src="https://images.pexels.com/photos/16009515/pexels-photo-16009515.jpeg"
                   className="block size-full object-cover"
                 />
               </div>
@@ -291,7 +291,7 @@ export default function Mission() {
               >
                 <img
                   alt=""
-                  src="https://images.unsplash.com/photo-1557804506-669a67965ba0?&auto=format&fit=crop&crop=left&w=560&h=560&q=90"
+                  src="https://images.unsplash.com/photo-1590650516494-0c8e4a4dd67e?&auto=format&fit=crop&crop=center&w=560&h=560&q=90"
                   className="block size-full object-cover"
                 />
               </div>
@@ -303,7 +303,7 @@ export default function Mission() {
               >
                 <img
                   alt=""
-                  src="https://images.unsplash.com/photo-1559136555-9303baea8ebd?&auto=format&fit=crop&crop=left&w=560&h=560&q=90"
+                  src="https://images.pexels.com/photos/5716031/pexels-photo-5716031.jpeg"
                   className="block size-full object-cover"
                 />
               </div>
