39713ad08050f8630ffacc537611df9b3f2b92e5
Gurung-Manish <manishgrg496@gmail.com>
2025-06-21 11:57:44 +0100

    updating abut page

--- src/app/home/components/hero.tsx
+++ src/app/home/components/hero.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Link from "next/link";
 
 gsap.registerPlugin(ScrollTrigger);
 export default function Hero() {
@@ -153,13 +154,13 @@ export default function Hero() {
                   Custom KPIs for What Matters Most to You. Our experts will
                   help your business to thrive with confidence
                 </p>
-                <a
+                <Link
                   ref={buttonRef}
-                  href="#"
+                  href="/contact"
                   className="w-fit rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Get started
-                </a>
+                </Link>
               </div>
             </div>
 
