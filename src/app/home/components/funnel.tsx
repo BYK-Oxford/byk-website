7a704206eb464c1708d83255281cacf0b8770c5d
Gurung-Manish <manishgrg496@gmail.com>
2025-06-29 10:03:12 +0100

    updated prod error

--- src/app/home/components/funnel.tsx
+++ src/app/home/components/funnel.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { gsap } from "gsap";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
